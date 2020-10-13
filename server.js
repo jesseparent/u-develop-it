5fec37a1eb78b728bc1c532ed492f0ab90d19082
Jesse Parent <jesseparent@gmail.com>
2020-10-12 22:08:03 -0600

    SQL from Express achieved

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,55 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = new sqlite3.Database('./db/election.db', err => {
+  if (err) {
+    return console.error(err.message);
+  }
+
+  console.log('Connected to the election database.');
+});
+
+// db.all(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
+
+// GET a single candidate
+db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+  if(err) {
+    console.log(err);
+  }
+  console.log(row);
+});
+
+// Delete a candidate - RUN expects no responses
+// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this, this.changes);
+// }); 
+
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
+// ES5 function, not arrow function, to use this
+// db.run(sql, params, function(err, result) {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result, this.lastID);
+// });
+
 // Default response for any other request(Not Found) Catch all
 app.use((req, res) => {
   res.status(404).end();
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start server after DB connection
+db.on('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
